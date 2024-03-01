cf74afd049cf94170cbf2018c335d2a629990c9e
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-03-01 10:45:10 -0500

    creacion del proyecto

---
+++ programacionoo/src/main/java/com/mycompany/programacionoo/Programacionoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacionoo;
+
+/**
+ *
+ * @author SCIS2PC29
+ */
+public class Programacionoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
