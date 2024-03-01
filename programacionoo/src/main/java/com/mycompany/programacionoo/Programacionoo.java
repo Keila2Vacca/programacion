9c18e67930a232147f373ec16669460182a558ec
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-03-01 11:33:50 -0500

    creacion de personajes por clase

--- programacionoo/src/main/java/com/mycompany/programacionoo/Programacionoo.java
+++ programacionoo/src/main/java/com/mycompany/programacionoo/Programacionoo.java
@@ -1,16 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.programacionoo;
 
-/**
- *
- * @author SCIS2PC29
- */
+
 public class Programacionoo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+     
+     Character homero = new Character("Homero","Inspector de seguridad",33,"A la grande le puse cuca");
+     homero.greeting();
+     
+     Character bart = new Character("Bartolomeo","Vago",10,"Ayyy caramba");
+     bart.greeting();
+     
+    }
+    
+    static class Character{
+        String name;
+        String occupation;
+        int age;
+        String phrase;
+
+        public Character(String name, String occupation, int age, String phrase) {
+            this.name = name;
+            this.occupation = occupation;
+            this.age = age;
+            this.phrase = phrase;
+        }
+        
+        
+       void greeting(){
+           System.out.println(this.phrase);  
+       }
     }
 }
