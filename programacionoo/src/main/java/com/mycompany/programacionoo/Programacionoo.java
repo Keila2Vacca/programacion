58c2c3cc7cd49253a5f9e0059e9558ba991cd469
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-03-01 12:06:00 -0500

    Creacion de paquetes, getters y settters con sobrecarga de constructor

--- programacionoo/src/main/java/com/mycompany/programacionoo/Programacionoo.java
+++ programacionoo/src/main/java/com/mycompany/programacionoo/Programacionoo.java
@@ -2,35 +2,19 @@
 
 package com.mycompany.programacionoo;
 
-
+import characters.Character;
+        
 public class Programacionoo {
 
     public static void main(String[] args) {
      
      Character homero = new Character("Homero","Inspector de seguridad",33,"A la grande le puse cuca");
      homero.greeting();
+        System.out.println(homero.getAge());
      
-     Character bart = new Character("Bartolomeo","Vago",10,"Ayyy caramba");
+     Character bart = new Character();
+     bart.setPhrase("Ayy caramba!!");
      bart.greeting();
      
-    }
-    
-    static class Character{
-        String name;
-        String occupation;
-        int age;
-        String phrase;
-
-        public Character(String name, String occupation, int age, String phrase) {
-            this.name = name;
-            this.occupation = occupation;
-            this.age = age;
-            this.phrase = phrase;
-        }
-        
-        
-       void greeting(){
-           System.out.println(this.phrase);  
-       }
     }
 }
